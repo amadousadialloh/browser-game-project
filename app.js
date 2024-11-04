d441ad7d7a401e283023d395c1ba33a197e9e4f2
amadousadialloh <amadousadialloh@gmail.com>
2024-11-04 06:42:04 -0600

    add a rest button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const spanElement = document.querySelector("span");
 const cookieImage = document.querySelector("img");
+const resetButton = document.querySelector(".reset");
 
 spanElement.style.margin = "0 5px";
 spanElement.style.color = "wheat";
@@ -10,4 +11,11 @@ const updateCookie = () => {
   spanElement.textContent = count;
   count++;
 };
+
+const reset = () => {
+  count = 0;
+  spanElement.textContent = count;
+};
+
 cookieImage.addEventListener("click", updateCookie); // add click event listener to the cookie image
+resetButton.addEventListener("click", reset); // rest cookie
